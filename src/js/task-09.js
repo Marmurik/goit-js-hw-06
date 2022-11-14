4c050d0ba38399c269d3002fb2fc4ec19213409c
Anastasia Marmura <anastasia.marmura@gmail.com>
2022-11-14 09:43:14 +0200

    task9_done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const colorSpanEl = document.querySelector('.color');
+const btnChangeEl = document.querySelector('.change-color');
+
+btnChangeEl.addEventListener('click', onBtnColorChange)
+
+function onBtnColorChange (event) {
+const randomColor = getRandomHexColor();
+bodyEl.style.backgroundColor = randomColor;
+colorSpanEl.textContent = randomColor;
+};
+
