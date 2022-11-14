5b615e89e3fb743de7dbceb2cb05e007ddb2c8b9
Anastasia Marmura <anastasia.marmura@gmail.com>
2022-11-14 12:44:23 +0200

    task1_update

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,11 +1,22 @@
-const categoriesNumberEl = document.querySelectorAll('li.item');
-console.log ('Number of categories:', categoriesNumberEl.length);
+const categories = document.querySelectorAll('.item')
+console.log ('Number of categories', categories.length)
+console.log('')
+categories.forEach((element) => {
+    console.log('Category:', element.firstElementChild.textContent)
+    console.log('Elements:', element.lastElementChild.children.length)
+    console.log('')
+})
 
-for (const category of categoriesNumberEl) {
-    const categoryName = category.querySelector('h2');
-    console.log('Category:', categoryName.textContent);
-    const listName = categoryName.nextElementSibling;
-    console.log('Elements:', listName.children.length);
-}
+
+// const categoriesNumberEl = document.querySelectorAll('li.item');
+// console.log ('Number of categories:', categoriesNumberEl.length);
+
+// for (const category of categoriesNumberEl) {
+//     const categoryName = category.querySelector('h2');
+//     console.log('Category:', categoryName.textContent);
+//     const listName = categoryName.nextElementSibling;
+//     console.log('Elements:', listName.children.length);
+// }
+
 
 
