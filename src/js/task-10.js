7532edf9d156ba834dceb84f8cef37ff80d161b1
Anastasia Marmura <anastasia.marmura@gmail.com>
2022-11-14 10:04:54 +0200

    task10_in process

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputFieldEl = document.querySelector('#controls>number');
+const createBtnEl = document.querySelector('button[data-create]');
+const destroyBtnEl = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+createBtnEl.addEventListener('click', onInputButtonChange);
+
+function onInputButtonChange (event){
+const randomColor = getRandomHexColor()
+
+}
